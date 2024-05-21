cf94b2febabcb4be1184134a5d61d17db3797379
boss <rakhimgaliev56@gmail.com>
2024-05-21 19:04:57 +0300

    fix: disabled creating template survey

--- apps/web/app/api/v1/client/[environmentId]/in-app/sync/[userId]/route.ts
+++ apps/web/app/api/v1/client/[environmentId]/in-app/sync/[userId]/route.ts
@@ -1,4 +1,4 @@
-import { getExampleSurveyTemplate } from "@/app/(app)/environments/[environmentId]/surveys/templates/templates";
+// import { getExampleSurveyTemplate } from "@/app/(app)/environments/[environmentId]/surveys/templates/templates";
 import { sendFreeLimitReachedEventToPosthogBiWeekly } from "@/app/api/v1/client/[environmentId]/in-app/sync/lib/posthog";
 import { responses } from "@/app/lib/api/response";
 import { transformErrorToDetails } from "@/app/lib/api/validator";
@@ -8,14 +8,14 @@ import { getActionClasses } from "@formbricks/lib/actionClass/service";
 import {
   IS_FORMBRICKS_CLOUD,
   PRICING_APPSURVEYS_FREE_RESPONSES,
-  PRICING_USERTARGETING_FREE_MTU,
-  WEBAPP_URL,
+  PRICING_USERTARGETING_FREE_MTU, // WEBAPP_URL,
 } from "@formbricks/lib/constants";
 import { getEnvironment, updateEnvironment } from "@formbricks/lib/environment/service";
 import { createPerson, getIsPersonMonthlyActive, getPersonByUserId } from "@formbricks/lib/person/service";
 import { getProductByEnvironmentId } from "@formbricks/lib/product/service";
 import { COLOR_DEFAULTS } from "@formbricks/lib/styling/constants";
-import { createSurvey, getSyncSurveys, transformToLegacySurvey } from "@formbricks/lib/survey/service";
+// import { createSurvey, getSyncSurveys, transformToLegacySurvey } from "@formbricks/lib/survey/service";
+import { getSyncSurveys, transformToLegacySurvey } from "@formbricks/lib/survey/service";
 import {
   getMonthlyActiveTeamPeopleCount,
   getMonthlyTeamResponseCount,
@@ -73,8 +73,8 @@ export async function GET(
       throw new Error("Environment does not exist");
     }
     if (!environment?.widgetSetupCompleted) {
-      const firstSurvey = getExampleSurveyTemplate(WEBAPP_URL);
-      await createSurvey(environmentId, firstSurvey);
+      // const firstSurvey = getExampleSurveyTemplate(WEBAPP_URL);
+      // await createSurvey(environmentId, firstSurvey);
       await updateEnvironment(environment.id, { widgetSetupCompleted: true });
       await updateUser(userId, { onboardingCompleted: true });
     }
--- apps/web/app/api/v1/client/[environmentId]/in-app/sync/route.ts
+++ apps/web/app/api/v1/client/[environmentId]/in-app/sync/route.ts
@@ -1,4 +1,4 @@
-import { getExampleSurveyTemplate } from "@/app/(app)/environments/[environmentId]/surveys/templates/templates";
+// import { getExampleSurveyTemplate } from "@/app/(app)/environments/[environmentId]/surveys/templates/templates";
 import { sendFreeLimitReachedEventToPosthogBiWeekly } from "@/app/api/v1/client/[environmentId]/in-app/sync/lib/posthog";
 import { responses } from "@/app/lib/api/response";
 import { transformErrorToDetails } from "@/app/lib/api/validator";
@@ -7,13 +7,13 @@ import { NextRequest } from "next/server";
 import { getActionClasses } from "@formbricks/lib/actionClass/service";
 import {
   IS_FORMBRICKS_CLOUD,
-  PRICING_APPSURVEYS_FREE_RESPONSES,
-  WEBAPP_URL,
+  PRICING_APPSURVEYS_FREE_RESPONSES, // WEBAPP_URL,
 } from "@formbricks/lib/constants";
 import { getEnvironment, updateEnvironment } from "@formbricks/lib/environment/service";
 import { getProductByEnvironmentId } from "@formbricks/lib/product/service";
 import { COLOR_DEFAULTS } from "@formbricks/lib/styling/constants";
-import { createSurvey, getSurveys, transformToLegacySurvey } from "@formbricks/lib/survey/service";
+// import { createSurvey, getSurveys, transformToLegacySurvey } from "@formbricks/lib/survey/service";
+import { getSurveys, transformToLegacySurvey } from "@formbricks/lib/survey/service";
 import { getMonthlyTeamResponseCount, getTeamByEnvironmentId } from "@formbricks/lib/team/service";
 import { isVersionGreaterThanOrEqualTo } from "@formbricks/lib/utils/version";
 import { TLegacySurvey } from "@formbricks/types/LegacySurvey";
@@ -77,8 +77,8 @@ export async function GET(
     }
 
     if (!environment?.widgetSetupCompleted) {
-      const firstSurvey = getExampleSurveyTemplate(WEBAPP_URL);
-      await createSurvey(environmentId, firstSurvey);
+      // const firstSurvey = getExampleSurveyTemplate(WEBAPP_URL);
+      // await createSurvey(environmentId, firstSurvey);
       await updateEnvironment(environment.id, { widgetSetupCompleted: true });
     }
 
