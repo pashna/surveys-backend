2235c599c4103cd92b092cd2256a4f107d66dd40
boss <rakhimgaliev56@gmail.com>
2024-07-08 04:19:43 +0300

    Mobile sdk: remove segment filter in mobile sync api

--- apps/web/app/api/v1/client/[environmentId]/mobile/sync/route.ts
+++ apps/web/app/api/v1/client/[environmentId]/mobile/sync/route.ts
@@ -1,14 +1,10 @@
 import { responses } from "@/app/lib/api/response";
 import { transformErrorToDetails } from "@/app/lib/api/validator";
 
-import { getActionClasses } from "@formbricks/lib/actionClass/service";
 import { getEnvironment, updateEnvironment } from "@formbricks/lib/environment/service";
-import { getProductByEnvironmentId } from "@formbricks/lib/product/service";
-import { COLOR_DEFAULTS } from "@formbricks/lib/styling/constants";
 import { getMobileSurveys } from "@formbricks/lib/survey/service";
 import { getTeamByEnvironmentId } from "@formbricks/lib/team/service";
 import { TJsStateSync, ZJsPublicSyncInput } from "@formbricks/types/js";
-import { TProduct } from "@formbricks/types/product";
 
 export async function OPTIONS(): Promise<Response> {
   return responses.successResponse({}, true);
@@ -46,37 +42,16 @@ export async function GET(_, { params }: { params: { environmentId: string } }):
       await updateEnvironment(environment.id, { widgetSetupCompleted: true });
     }
 
-    const [surveys, noCodeActionClasses, product] = await Promise.all([
-      getMobileSurveys(environmentId),
-      getActionClasses(environmentId),
-      getProductByEnvironmentId(environmentId),
-    ]);
-    if (!product) {
-      throw new Error("Product not found");
-    }
+    const [surveys] = await Promise.all([getMobileSurveys(environmentId)]);
 
     // Common filter condition for selecting surveys that are in progress, are of type 'web' and have no active segment filtering.
-    let filteredSurveys = surveys.filter(
-      (survey) =>
-        survey.status === "inProgress" &&
-        survey.type === "mobile" &&
-        (!survey.segment || survey.segment.filters.length === 0)
+    let filteredSurveys: TJsStateSync["surveys"] = surveys.filter(
+      (survey) => survey.status === "inProgress" && survey.type === "mobile"
     );
 
-    const updatedProduct: TProduct = {
-      ...product,
-      brandColor: product.styling.brandColor?.light ?? COLOR_DEFAULTS.brandColor,
-      ...(product.styling.highlightBorderColor?.light && {
-        highlightBorderColor: product.styling.highlightBorderColor.light,
-      }),
-    };
-
-    // Create the 'state' object with surveys, noCodeActionClasses, product, and person.
-    const state: TJsStateSync = {
+    // Create the 'state' object with surveys
+    const state = {
       surveys: filteredSurveys,
-      noCodeActionClasses: noCodeActionClasses.filter((actionClass) => actionClass.type === "noCode"),
-      product: updatedProduct,
-      person: null,
     };
 
     return responses.successResponse(
