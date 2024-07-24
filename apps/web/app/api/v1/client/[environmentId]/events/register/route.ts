fc934bc12ce8c012e22542106fd81ecf2fd94518
boss <rakhimgaliev56@gmail.com>
2024-07-24 16:12:25 +0300

    feat: create action classes via api

---
+++ apps/web/app/api/v1/client/[environmentId]/events/register/route.ts
@@ -0,0 +1,49 @@
+import { responses } from "@/app/lib/api/response";
+import { transformErrorToDetails } from "@/app/lib/api/validator";
+
+import { createActionClass } from "@formbricks/lib/actionClass/service";
+import { ZActionClassInput } from "@formbricks/types/actionClasses";
+
+interface Context {
+  params: {
+    environmentId: string;
+  };
+}
+
+export async function OPTIONS(): Promise<Response> {
+  return responses.successResponse({}, true);
+}
+
+export async function POST(req: Request, context: Context): Promise<Response> {
+  try {
+    const jsonInput = await req.json();
+    const name = jsonInput?.name;
+    const environmentId = context.params.environmentId;
+
+    // validate using zod
+    const inputValidation = ZActionClassInput.safeParse({
+      name,
+      type: "automatic",
+      environmentId: context.params.environmentId,
+    });
+
+    if (!inputValidation.success) {
+      return responses.badRequestResponse(
+        "Fields are missing or incorrectly formatted",
+        transformErrorToDetails(inputValidation.error),
+        true
+      );
+    }
+
+    const actionClass = await createActionClass(environmentId, {
+      name,
+      type: "automatic",
+      environmentId,
+    });
+
+    return responses.successResponse(actionClass, true);
+  } catch (error) {
+    console.error(error);
+    return responses.internalServerErrorResponse("Unable to handle the request: " + error.message, true);
+  }
+}
